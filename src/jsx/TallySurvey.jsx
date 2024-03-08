77549d4b4fde68363f4ded0fe920b56e5a6d0014
ghtlsh <143321578+ghtlsh@users.noreply.github.com>
2024-03-08 05:41:11 +0000

    css-tally

--- src/jsx/TallySurvey.jsx
+++ src/jsx/TallySurvey.jsx
@@ -39,15 +39,15 @@ const TallySurvey = () => {
 
   return (
     <div>
-     <iframe 
-      data-tally-src="https://tally.so/embed/mKldzV?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" 
-      loading="lazy" 
-      width="100%" 
-      height="899" 
-      frameborder="0" 
-      marginheight="0"
-      marginwidth="0" 
-      title="null"></iframe>
+      <iframe 
+        data-tally-src="https://tally.so/embed/mKldzV?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" 
+        loading="lazy" 
+        width="100%" 
+        height="200" 
+        frameborder="0" 
+        marginheight="0" 
+        marginwidth="0" 
+        title="null"></iframe>
     </div>
   );
 };
